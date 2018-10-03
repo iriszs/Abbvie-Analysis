1e9c8a36da301ab3e75330fb80b30419719676c0
iriszs <i.m.gorter@st.hanze.nl>
2018-10-03 14:31:03 +0200

    added a quality control script

---
+++ 2-QC.R
@@ -0,0 +1,58 @@
+# Remove all environmental variables
+rm(list=ls(all=TRUE))
+# Restart R to make sure a 'clean' environment is created and all used memory is freed
+.rs.restartR()
+
+########################################################
+#                      Sources                         #
+########################################################
+
+# Load script that contains the used functions in this script
+source("/media/imgorter/BD_1T/Iris/Scripts/abbvie/packageTest.R")
+
+########################################################
+#                     Options                          #
+########################################################
+
+options(stringsAsFactors = FALSE)
+# Pseudo-random number generation to insure results can be reproduced
+set.seed(1234567)
+
+########################################################
+#                     Packages                         #
+########################################################
+
+# Load only the necessary packages for this script
+# With the use of the pkgTest and bioPkgTest functions from the packageTest.r file
+
+bioPkgTest("SingleCellExperiment")
+bioPkgTest("scater")
+
+########################################################
+#                  Quality Control                     #
+########################################################
+
+# Load the SCE object from the first script
+sceset <- readRDS("/media/imgorter/BD_1T/Iris/Scripts/abbvie/RDS/SCE.rds")
+
+# Calculate quality metrics for each cell and feature
+sceset <- calculateQCMetrics(sceset)
+
+# Create histogram of the RNA molecules distribution across cells
+hist(sceset$total_counts, breaks = 100, main = "Total number of RNA molecules per cell", xlab = "Amount of RNA molecules")
+
+# Create histogram of the unique genes distribution across cells
+hist(sceset$total_features, breaks = 100, main = "Total number of unique genes detected per cell", xlab = "Amount of genes")
+
+# Filters based on RNA molecules and unique genes
+# are 'off' for now, but not removed to make it easy to change it fast
+filter_by_total_counts <- (sceset$total_counts > 0)
+
+filter_by_expr_features <- (sceset$total_features > 0)
+
+# Only use cells that pass the filter
+sceset$use <- (filter_by_expr_features & filter_by_total_counts)
+
+# Save object to import in another script
+saveRDS(sceset, "/media/imgorter/BD_1T/Iris/Scripts/abbvie/RDS/SCE_QC.rds" )
+
