a7400e264dfaa10c96580382bc2091bf65841b3c
iriszs <i.m.gorter@st.hanze.nl>
2018-11-08 13:49:55 +0100

    Changed the data-loading method of the celltypeidentification script

---
+++ packageTest.R
@@ -0,0 +1,27 @@
+# This script is imported in other scripts 
+# It contains two functions to prevent unnecessary installation of packages 
+
+########################################################
+#                     Packages                         #
+########################################################
+
+# Function to test if packages that need to be loaded are already installed.
+# If not installed, install using install.packages()
+
+pkgTest <- function(x){
+  if (suppressMessages(!require(x,character.only = TRUE, quietly = TRUE))){
+    install.packages(x, repos = "http://cran.us.r-project.org")
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
+
+# Function to test if bioconductor packages that need to be loaded are already installed
+# If not installed, install using biocLite()
+
+bioPkgTest <- function(x){
+  if (suppressMessages(!require(x,character.only = TRUE, quietly = TRUE))){
+    suppressMessages(source("https://bioconductor.org/biocLite.R"))
+    biocLite(x)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
+}
