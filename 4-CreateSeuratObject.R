950f6736fac1a6529b5724e262af6a16d16ff134
iriszs <i.m.gorter@st.hanze.nl>
2018-12-04 14:50:44 +0100

    Changes the way of adding metadata to the seurat object

--- 4-CreateSeuratObject.R
+++ 4-CreateSeuratObject.R
@@ -38,8 +38,6 @@ samples <- c("WT1-cells", "WT1-nuclei", "WT2-cells", "WT2-nuclei", "APP1-cells",
 # Source of loading in data: https://ucdavis-bioinformatics-training.github.io/2017_2018-single-cell-RNA-sequencing-Workshop-UCD_UCB_UCSF/day2/scRNA_Workshop-PART1.html
 data <- sapply(samples, function(i){
   d10x <- Read10X(file.path(dataset_loc, i, "outs/filtered_gene_bc_matrices/mm10/"))
-  # Add samplename to the colnames
-  colnames(d10x) <- paste(sapply(strsplit(colnames(d10x),split="-"),'[[',1L),i,sep="-")
   d10x
 })
 
@@ -53,22 +51,13 @@ seuset <- CreateSeuratObject(
   names.field = 2,
   names.delim = "\\-")
 
+
 # Read in metadata
 meta.data <- read.csv("/media/imgorter/BD_1T/Iris/Data_abbvie/reference_AD_WT.csv", header = TRUE)
 
-# For each column in the metadata file
-for (j in 1:ncol(meta.data)){
-  # Create a dataframe with the lenght of the data in the Seurat object
-  meta <- data.frame(matrix(NA, nrow = length(colnames(seuset@raw.data)), ncol = 1))
-  # Add all meta data rows to the dataframe
-  t <- meta.data[names(samples[1]),]
-  variable <- as.character(t[,j])
-  vector <- c(rep(variable, length(seuset@cell.names)))
-  meta[,1] <- as.factor(vector)
-  colnames(meta) <- c(as.character(colnames(t[j])))
-  rownames(meta) <- colnames(seuset@raw.data)
-  seuset <- AddMetaData(seuset, metadata = meta)
-}
+meta.data$Barcode <- unlist(strsplit(meta.data$Barcode, split="-1"))
+
+seuset <- AddMetaData(seuset, metadata = meta.data)
 
 # Save Seurat object
 saveRDS(seuset, file = "/media/imgorter/BD_1T/Iris/Scripts/abbvie/RDS/Seurat.rds")
