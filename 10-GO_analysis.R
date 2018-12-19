0d42beda69a8674c6ce0ee7dc65a2458cd07cbe0
iriszs <i.m.gorter@st.hanze.nl>
2018-12-19 14:41:44 +0100

    Added comments to the GO script and added code to remove ribosomal and mitochondrial genes

--- 10-GO_analysis.R
+++ 10-GO_analysis.R
@@ -31,30 +31,95 @@ bioPkgTest("edgeR")
 bioPkgTest("scater")
 pkgTest("xlsx")
 
-
 ########################################################
 #                     GO Analysis                      #
 ########################################################
 
+# Load RDS
 seuset <- readRDS("/media/imgorter/BD_1T/Iris/Scripts/abbvie/RDS/cells_DAM.rds")
 
+# Get all cells that are assigned to cluster 0
 damSet <- SubsetData(seuset, ident.use = 0)
 
+# Calculate RPKM values for the counts based on the total number of genes
 x_rpkm <- rpkm(damSet@raw.data, 28692)
 rpkmsum <- sum(x_rpkm, na.rm=F)
 tpm.values <- x_rpkm/rpkmsum * 10^6
 
+# Set the RPKM values as the data assay
 damSet@data <- tpm.values
 
+# Calculate the 25th percentile 
 percentile <- damSet@data[rowSums(damSet@data >0) > quantile(rowSums(damSet@data) , probs=0.75),]
 
+# Get the rownames (which are the gene symbols) of the 25th percentile
 symbol <- rownames(percentile)
 
+# Put those genes in a dataframe
 genes.df <- as.data.frame(symbol)
 
+# Convert the gene symbols to entrez id's and add to the genes dataframe
 genes.df[,"entrez"] <- mapIds(org.Mm.eg.db, keys = genes.df$symbol, column="ENTREZID", keytype = "SYMBOL", multiVals="first")
 
+# perform GO Enrichment Analysis of the entrez gene set with the mouse database
 GOEA <- enrichGO(as.character(genes.df$entrez), OrgDb="org.Mm.eg.db")
 
+# Plot the results in a dotplot
 dotplot(GOEA)
 
+# Calculate the mean expression of each gene
+mean <- rowMeans(damSet@data)
+mean <- as.data.frame(mean)
+# Order the expression from highest to lowest
+mean <- mean[order(-mean$mean), , drop = FALSE]
+
+# Write the 3000 genes that have the average highest expression to an xlsx file to use in Metascape
+write.xlsx(rownames(mean)[0:3000], file = "/media/imgorter/BD_1T/Iris/Results/DAM_analysis/GO_analysis/first_3000.xlsx", row.names = FALSE, col.names = FALSE)
+
+###################################################################################################################
+
+# Convert the RPKM values and genes to a single cell experiment object
+# Since it is not possible to remove genes from a Seurat object
+sceset <- SingleCellExperiment(assays = list(counts = damSet@data))
+
+# Find the mitochondrial genes (those genes start with mt-)
+is.mito <- grepl("^mt-", rownames(sceset))
+
+# Remove those genes from the sceset
+sceset <- sceset[is.mito==FALSE, ]
+
+# Find the ribosomal genes(those genes start with Rp and have a number in them)
+is.ribo <- grepl("^Rp[sl][[:digit:]]", rownames(sceset))
+
+# Remove the ribosomal genes from the sceset
+sceset <- sceset[is.ribo==FALSE, ]
+
+# Calculate how many genes are removed
+length(which(is.mito)) + length(which(is.ribo))
+
+# Calculate the 25th percentile again, now with 122 genes less
+percentile <- assay(sceset, "counts")[rowSums(assay(sceset, "counts")) > quantile(rowSums(assay(sceset, "counts")) , probs=0.75),]
+
+# Get the rownames (which are the gene symbols) of the 25th percentile
+symbol <- rownames(percentile)
+
+# Put the genes in a dataframe
+genes.df <- as.data.frame(symbol)
+
+# Convert the gene symbols to entrez id's
+genes.df[,"entrez"] <- mapIds(org.Mm.eg.db, keys = genes.df$symbol, column="ENTREZID", keytype = "SYMBOL", multiVals="first")
+
+# perform GO Enrichment Analysis of the entrez gene set with the mouse database
+GOEA <- enrichGO(as.character(genes.df$entrez), OrgDb="org.Mm.eg.db")
+
+# Plot the results in a dotplot
+dotplot(GOEA)
+
+# Calculate the mean expression of each gene
+mean <- rowMeans(assay(sceset, "counts"))
+mean <- as.data.frame(mean)
+# Order the expression from highest to lowest
+mean <- mean[order(-mean$mean), , drop = FALSE]
+
+# Write the 3000 genes that have the average highest expression to an xlsx file to use in Metascape
+write.xlsx(rownames(mean)[0:3000], file = "/media/imgorter/BD_1T/Iris/Results/DAM_analysis/GO_analysis/first_3000_without_ribo_mito.xlsx", row.names = FALSE, col.names = FALSE)
